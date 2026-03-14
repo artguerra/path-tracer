eabe1f8be28ace1d1cf6e254ae0471dfa034488f
Arthur Guerra <conta.arthurguerra@gmail.com>
2026-03-14 17:02:53 +0100

    add base path to vite build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
+  base: "rendering-playground",
   resolve: {
     alias: {
       "@shaders": path.resolve(__dirname, "./shaders/"),
